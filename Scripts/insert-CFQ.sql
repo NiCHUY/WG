a65cb8aef982b4f120aa73e33e0d786f58e9251f
NiCHUY <89972933+NiCHUY@users.noreply.github.com>
2023-05-31 16:42:50 +0300

    Add files via upload

---
+++ Scripts/insert-CFQ.sql
@@ -0,0 +1,4 @@
+INSERT INTO CompareFactQuiz
+(CompareFactQuiz_ID, Question1, Question2, Question3, Question4, Variant1, Variant2, Variant3, Variant4, Answer1, Answer2, Answer3, Answer4)
+VALUES
+(1, N'What is the capital city of France?', N'What is the capital city of Germany?', N'What is the capital city of Italy?', N'What is the capital city of Spain?', 1, 2, 3, 4, 1, 2, 3, 4);
---
+++ Scripts/insert-country.sql
@@ -0,0 +1,4 @@
+INSERT INTO Country
+(Country_ID, Name, Area, Population, Continent, Fact, Flag, Territory)
+VALUES
+(1, 'Canada', '9984670', 38008005, 'North America', 'Canada has the longest coastline of any country in the world, stretching over 202,080 km.', 'canada_f', 'canada_t');
